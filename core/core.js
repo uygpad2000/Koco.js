ef549b8a44afa90e030f6735e97e10d795a7a195
Uygpad2000 <121656455+uygpad2000@users.noreply.github.com>
2023-06-06 18:46:05 +0800

    Update core.js

--- core/core.js
+++ core/core.js
@@ -1,24 +1,47 @@
-/**
- * Create a root task container to hold all the widgets and renderers in a Koco block.
- * 
- * @param {Object} options - An object with optional properties that will be attached to the task.
- * @param {Function} returns - A constructor function that specifies what Core should return.
- * 
- * @returns {Object} - A task object for subsequent functions and events.
- */
+function h(tag,a,b,c){
 
-export function createTask(options, returns) {
-    const task = {};
+  var data;
+  var text;
+  var parent;
+  var children;
 
-    task.tracker = true; // enable task tracker
-    
-    if (options) {
-      Object.keys(options).forEach(key => {
-        // attach all provided options to the task
-        task[key] = options[key];
-      });
+  if (b != undefined){
+    if (a != null){
+      data = a;
     }
-    
-    return task;
+    if (Array.isArray(b)){
+      children=b;
+    }
+    if (typeof b === 'string' || typeof b === 'number'){
+      text = b;
+    }
   }
+
+  else if (a != undefined && a !== null){
+    if (Array.isArray(a)){
+      children = a;
+    }
+    if (typeof a === 'string' || typeof a === 'number'){
+      text = a;
+    }
+  }
+
+  if (c && c.tag){
+    parent = c;
+  }
+
+  return vn(tag,data,children,parent,text);
+
+}
+
+function vn(tag,data,children,parent,text) {
   
+  var identifier;
+
+  if (data !== null && typeof data === 'object'){
+    identifier = data.identifier;
+  }
+
+  return {tag,data,text,children,parent,identifier};
+
+}
